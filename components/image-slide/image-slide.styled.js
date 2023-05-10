58b0c2a06590925bea11434fa50fc4fa5efedf38
supawit.k <supawit.kli@ttbspark.com>
2023-05-10 18:51:06 +0700

    add slide

---
+++ components/image-slide/image-slide.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const SlideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`
